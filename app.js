a278de2593bf0bd5117f52b666fb570d5d800a55
adam <adam2.chojnowski@gmail.com>
2017-06-09 16:25:30 +0200

    bazowy routing

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var resultsFilter = require('./routes/resultsFilter');
+var person = require('./routes/person');
+
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/results', resultsFilter);
+app.use('/person', person);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
