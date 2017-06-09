f72670379e264c6b506772cc5516251c006905b5
adam <adam2.chojnowski@gmail.com>
2017-06-09 22:14:05 +0200

    uporządkowanie backendu

---
+++ travbuddy/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template('main_filter.html')
+
+@app.route("/results")
+def results_page():
+    return render_template('results_filter.html')
+
+@app.route("/person/<id>")
+def person_page(id):
+    return render_template('person.html')
+
+if __name__ == "__main__":
+    app.run()
