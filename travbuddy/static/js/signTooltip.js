7ef96ac7ca15a7f9a38bd3addfc9a53252958655
adam <adam2.chojnowski@gmail.com>
2017-06-10 00:39:53 +0200

    tooltip z miganiem

--- travbuddy/static/js/signTooltip.js
+++ travbuddy/static/js/signTooltip.js
@@ -1,3 +1,9 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
+    $('.signLangToggler').click(function(){
+        $('.signTooltip').toggle();
+    });
+    $('.highContrastToggler').click(function(){
+        $('body').toggleClass('highContrast');
+    });
 });
